45f785b2c95818f56c1750eafc75c8ac0621bdb6
Princess Sampson <ps@kingonomics.com>
2015-07-07 10:14:34 -0400

    navbar. yay

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,29 @@
-console.log('Angular Rocks', angular);
+;(function (){
+  
+  'use strict';
+
+  var app = angular.module('LeetApp', ['ui.router', 'ngMaterial', 'angular-loading-bar']);
+
+    app.config([ '$stateProvider', '$urlRouterProvider', 
+
+      function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+          .state('home', {
+            url: '/',
+            templateUrl: 'js/templates/home.tpl.html',
+            controller: ''
+          })
+
+          .state('register', {
+            url: '/register',
+            templateUrl: 'js/templates/register.tpl.html',
+            controller: ''
+          });
+      }
+
+  ]);
+
+}());
