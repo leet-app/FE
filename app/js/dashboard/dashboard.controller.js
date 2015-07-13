12aacc3b782f6cf96e93ce8ca16222add7ca3768
Princess Sampson <ps@kingonomics.com>
2015-07-13 15:07:22 -0400

    sidebar toggles at click of a button!

--- app/js/dashboard/dashboard.controller.js
+++ app/js/dashboard/dashboard.controller.js
@@ -4,9 +4,29 @@
 
   angular.module('LeetApp')
 
-  .controller('DashboardCtrl', ['$scope', '$mdSidenav', 
+  .controller('DashboardCtrl', ['$scope', '$mdSidenav', '$timeout', '$log', '$mdUtil',
 
-    function ($scope, $mdSidenav) {
+    function ($scope, $mdSidenav, $timeout, $log, $mdUtil) {
+
+      $scope.toggleLeft = buildToggler('left');
+
+     function buildToggler(navID) {
+       var debounceFn =  $mdUtil.debounce(function(){
+             $mdSidenav(navID)
+               .toggle()
+               .then(function () {
+                 $log.debug("toggle " + navID + " is done");
+               });
+           },300);
+       return debounceFn;
+     }
+
+      $scope.close = function () {
+        $mdSidenav('left').close()
+          .then(function () {
+            $log.debug("close LEFT is done");
+          });
+      };
 
     }
   ]);
