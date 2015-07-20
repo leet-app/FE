c965fe00c70c762b18a89a736a92c2b553d9bf0a
Princess Sampson <ps@kingonomics.com>
2015-07-19 20:25:17 -0400

    had to move new layover logic to controller

--- app/js/dashboard/dashboard.controller.js
+++ app/js/dashboard/dashboard.controller.js
@@ -9,7 +9,22 @@
       function ($scope, DashboardService, $http, RB) {
         
         $scope.newLayover = function(layover) {
-          DashboardService.newLayover(layover);
+
+          // var newLayoverEndpoint = RB.URL + 'layovers/create';
+          
+          layover = {
+            short_name : layover.short_name,
+            city : layover.city,
+            arrival_time : layover.arrival_time,
+            dept_time : layover.dept_time,
+            display : layover.display
+          };
+
+          console.log('layover', layover);
+        };
+
+        $scope.getLayovers = function() {
+
         };
       }
     ]);
